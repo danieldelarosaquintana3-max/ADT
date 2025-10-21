78f2c63fa03745b4fec8289ad22ba4c39c0968d7
Daniel <danieldelarosaquintana3@gmail.com>
2025-10-21 19:12:58 +0200

    AAAAAAAAAAAAAAAAAAAAAAAAAA

--- ADT/src/Tests/Test1.java
+++ ADT/src/Tests/Test1.java
@@ -1,46 +1,29 @@
 package Tests;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
 
 public class Test1 {
 	public static void main(String[] args) { 
-		
-		Path p = Paths.get("C:\\Users\\danie\\Desktop\\test.txt");
-//		String line;
-//		
-//		try(BufferedReader bf = Files.newBufferedReader(p)) {
-//			while((line = bf.readLine()) != null) {
-//				System.out.println(line);
-//			}
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-		
-		
-		String s = "";
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		File f = new File("Test.txt");
 		try {
-			s = bf.readLine();
-		} catch (Exception e) {
-			// TODO: handle exception
+			BufferedReader br = new BufferedReader(new FileReader(f));
+			
+			String linea;
+			while((linea = br.readLine()) != null) {
+				System.out.println(linea);
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		try (BufferedWriter bw = Files.newBufferedWriter(p)){
-			bw.write(s);
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
-		
-		
 		
 	}
 }
--- ADT/src/Tests/Test2.java
+++ ADT/src/Tests/Test2.java
@@ -1,28 +1,16 @@
 package Tests;
 
-import java.io.BufferedWriter;
+import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.PrintWriter;
 
 public class Test2 {
-    private static final String vowels = "AEIOUaeiou";
 
     public static void main(String[] args) throws IOException {
-//    	var res =
-//        Files.lines(Paths.get("C:\\Users\\danie\\Desktop\\test.txt"))
-//            .flatMap(line -> Arrays.stream(line.split("")))
-//            .filter(ch -> vowels.contains(ch))
-//            .count();
-//    	
-//    	System.out.println(res);
-    	
-    	Path p = Paths.get("C:\\Users\\danie\\Desktop\\test.txt");
-    	BufferedWriter bw = Files.newBufferedWriter(p);
-    	bw.write("1234TEST");
-    	bw.flush();
-    	Files.lines(p)
-    		.forEach(t -> System.out.println(t));
+    	PrintWriter pw = new PrintWriter(new File("test.txt"));
+    	pw.println("AAAAAA");
+    	pw.println("AAAAAA");
+    	pw.println("AAAAAA");
+    	pw.flush();
     }
 }
